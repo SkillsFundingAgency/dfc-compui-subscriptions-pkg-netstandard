95fdab2bc0db6e00bd652d8d47c4056f2df578fa
Tom Tollafield <thomas.tollafield@gmail.com>
2020-07-07 15:12:48 +0100

    WIP

---
+++ DFC.Compui.Subscriptions.Pkg.Webhook/Controllers/SomeRandomController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DFC.Compui.Subscriptions.Pkg.Webhook.Controllers
+{
+    public class SomeRandomController : Controller
+    {
+        [HttpGet("Index")]
+        public IActionResult Index()
+        {
+            return new BadRequestObjectResult("Test");
+        }
+    }
+}
