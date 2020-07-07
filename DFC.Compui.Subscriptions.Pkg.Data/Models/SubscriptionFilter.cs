3343fd7fa6ae2f86befa103efeb51515486f5a95
Tom Tollafield <thomas.tollafield@gmail.com>
2020-07-07 12:53:58 +0100

    WIP

---
+++ DFC.Compui.Subscriptions.Pkg.Data/Models/SubscriptionFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DFC.Compui.Subscriptions.Pkg.Data
+{
+    [ExcludeFromCodeCoverage]
+    public class SubscriptionFilter
+    {
+        public string? BeginsWith { get; set; }
+
+        public string? EndsWith { get; set; }
+
+        public List<string>? IncludeEventTypes { get; set; }
+
+        public List<SubscriptionPropertyContainsFilter>? PropertyContainsFilters { get; set; }
+    }
+}
---
+++ DFC.Compui.Subscriptions.Pkg.Data/Models/SubscriptionPropertyContainsFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DFC.Compui.Subscriptions.Pkg.Data
+{
+    public class SubscriptionPropertyContainsFilter
+    {
+        public string? Key { get; set; }
+
+#pragma warning disable CA1819 // Properties should not return arrays
+        public string[]? Values { get; set; }
+#pragma warning restore CA1819 // Properties should not return arrays
+    }
+}
