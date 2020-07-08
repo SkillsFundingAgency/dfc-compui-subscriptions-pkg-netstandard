f7ade39b131e232a8a1c3927fe61770ce1987759
Tom Tollafield <thomas.tollafield@gmail.com>
2020-07-08 09:53:05 +0100

    WIP

---
+++ DFC.Compui.Subscriptions.Pkg.Data/Models/CuriesDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace DFC.Compui.Subscriptions.Pkg.Data.Models
+{
+    public class CuriesDetails
+    {
+        public string? Name { get; set; }
+
+        public string? Href { get; set; }
+    }
+}
