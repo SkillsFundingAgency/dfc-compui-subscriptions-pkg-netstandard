4c769473a82f2c7b820617a153425e44024e6ed9
Tom Tollafield <thomas.tollafield@gmail.com>
2020-07-24 15:35:06 +0100

    WIP

---
+++ DFC.Compui.Subscriptions.Pkg.Netstandard/Data/Models/ApiAdvancedFilter.cs
@@ -0,0 +1,11 @@
+﻿namespace DFC.Compui.Subscriptions.Pkg.NetStandard.Data.Models
+{
+    public class ApiAdvancedFilter
+    {
+        public string? Property { get; set; }
+
+        public FilterTypeEnum Type { get; set; }
+
+        public List<string>? Values { get; set; }
+    }
+}
