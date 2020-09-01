e31cf42aea3901f71095fdde78f756df698beb14
Tom Tollafield <thomas.tollafield@gmail.com>
2020-09-01 07:43:45 +0100

    - Remove Webhook related classes

--- DFC.Compui.Subscriptions.Pkg.NetStandard.UnitTests/WebhookTests/TestModels/CuriesDetails.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DFC.Compui.Subscriptions.Pkg.Webhook.UnitTests.TestModels
-{
-    public class CuriesDetails
-    {
-        public string? Name { get; set; }
-
-        public string? Href { get; set; }
-    }
-}
